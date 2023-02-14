b08c8d6391e52a8be2ad6c5c51a9fae8cc5be47d
louitron <luisgarcia1437@gmail.com>
2023-02-14 04:20:44 +0000

    Began working on logic using nested for loops

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      const combination = `${pronoun[i]} ${adj[j]} ${noun[k]}`;
+      console.log(combination);
+    }
+  }
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
