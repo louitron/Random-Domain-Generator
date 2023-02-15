18d5e2151f9b0c835a4ef3a124e209cdd2e8c545
louitron <luisgarcia1437@gmail.com>
2023-02-15 22:10:58 +0000

    added a graphic interface with text inputs, buttons which add text to arrays, and button to submit and generate all possible combiations. Also added a list element at the bottom which prints all possible combinations of domain names

--- src/app.js
+++ src/app.js
@@ -8,17 +8,27 @@ import "./assets/img/4geeks.ico";
 const pronoun = ["the", "our"];
 const adj = ["great", "big"];
 const noun = ["jogger", "racoon"];
+const possibilities = [];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       const combination = `${pronoun[i]} ${adj[j]} ${noun[k]}`;
-      console.log(combination);
+      possibilities.push(combination);
     }
   }
 }
 
+function addPossibilities() {
+  let template = possibilities
+    .map(element => `<li class="list-group-item">${element}</li>`)
+    .join("\n");
+  document.querySelector("ul").innerHTML = template;
+}
+
+console.log(possibilities);
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  addPossibilities();
 };
