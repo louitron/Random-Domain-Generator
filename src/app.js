e285ab3296450c22146117966f64dd4e57f019c5
louitron <luisgarcia1437@gmail.com>
2023-02-16 02:02:03 +0000

    began working on adding user input to the correct array, and generating all possible domain names including user input when generate button is clicked

--- src/app.js
+++ src/app.js
@@ -5,19 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const pronoun = ["the", "our"];
-const adj = ["great", "big"];
-const noun = ["jogger", "racoon"];
+const pronoun = ["the", "our", "His", "Their"];
+const adj = ["great", "big", "wild", "hairy"];
+const noun = ["jogger", "racoon", "pear", "orange"];
 const possibilities = [];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      const combination = `${pronoun[i]} ${adj[j]} ${noun[k]}`;
-      possibilities.push(combination);
+const genRanDomain = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        const combination = `${pronoun[i]} ${adj[j]} ${noun[k]}`;
+        possibilities.push(combination);
+      }
     }
   }
-}
+};
 
 function addPossibilities() {
   let template = possibilities
@@ -28,7 +30,18 @@ function addPossibilities() {
 
 console.log(possibilities);
 
+gsap.from(".button", {
+  duration: 0.5,
+  opacity: 0,
+  y: "40%",
+  ease: "power.4"
+});
+
+let pronounButton = document.querySelector(".addPronoun");
+let inputPronoun = document.querySelector(".addPronounInput");
+
 window.onload = function() {
   //write your code here
-  addPossibilities();
+
+  addPossibilities(genRanDomain());
 };
